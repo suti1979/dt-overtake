a564a05f8a07bad9bee376f76e39425f0afa20e0
Andras Zold <andras.zold@digitalthinkers.com>
2023-06-21 15:54:22 +0200

    added flegs

--- client/src/app/components/Card.tsx
+++ client/src/app/components/Card.tsx
@@ -2,6 +2,7 @@
 import { useRouter } from "next/navigation";
 import React from "react";
 import Image from "next/image";
+import "/node_modules/flag-icons/css/flag-icons.min.css";
 
 export default function Card(driver: Driver) {
   const router = useRouter();
@@ -22,14 +23,16 @@ export default function Card(driver: Driver) {
       className="flex flex-col items-center w-72  justify-center  bg-gray-200 rounded-lg text-black m-2 cursor-pointer"
       onClick={() => overtake(driver.id)}
     >
-      <div className="w-full pl-4 bg-slate-400 rounded-tl rounded-tr">
+      <div className="w-full pl-4 bg-slate-400 rounded-tl rounded-tr shadow font-mono">
         Place: #{driver.place}
       </div>
-      <div>
-        {driver.firstname} {driver.lastname}, {driver.code}
+      <div className=" font-bold ">
+        <span className={`fi fi-${driver.country.toLowerCase()}`}></span>
+        <span className="ml-2">
+          {driver.firstname} {driver.lastname}, {driver.code}
+        </span>
       </div>
-
-      <div>Team: {driver.team}</div>
+      <div>{driver.team}</div>
       <Image
         src={`${process.env.NEXT_PUBLIC_SERVER_URL}${driver.imgUrl}`}
         alt={driver.lastname}
