9b57f2f5c6c443d8f866c39dfacc5b27575e47b5
Andras Zold <andras.zold@digitalthinkers.com>
2023-06-22 08:33:47 +0200

    setup cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,14 +7,14 @@ import { join } from 'path';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // const corsOptions: CorsOptions = {
-  //   origin: 'http://localhost:3000/',
-  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  //   allowedHeaders: 'Content-Type,Authorization',
-  //   credentials: true,
-  // };
+  const corsOptions: CorsOptions = {
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
+    credentials: true,
+  };
 
-  app.enableCors();
+  app.enableCors(corsOptions);
   app.use('/static', express.static(join(__dirname, '..', 'public')));
 
   await app.listen(4000);
